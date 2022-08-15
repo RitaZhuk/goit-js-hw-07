9e050cf618b407f392f0da8103458149e45d154e
RitaZhuk <ritochka199097@gmail.com>
2022-08-15 22:28:32 +0300

    02 gallery

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const onGalleryContainer = document.querySelector('.gallery');
+const onItemsGallery = createItemsGallery(galleryItems);
+onGalleryContainer.insertAdjacentHTML('beforeend', onItemsGallery);
+function createItemsGallery(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+        <li>
+            <a class="gallery__item" href="${original}">
+            <img
+                class="gallery__image"
+                src="${preview}"
+                alt="${description}"
+                />
+                </a>
+                </li>
+        `
+    }).join('');
+}
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: "alt",
+    captionType: "alt",
+    captionDelay: 250,
+});
 
 console.log(galleryItems);
